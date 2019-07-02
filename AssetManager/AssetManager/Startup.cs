f230de9bc8a441b215a3891fca2a1d5e29f223a0
Md. Solaiman Shohag <solaiman.shohag95@gmail.com>
2019-07-03 00:49:24 +0600

    Initial State

---
+++ AssetManager/AssetManager/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(AssetManager.Startup))]
+namespace AssetManager
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
